0e782b43603ef4abf5ea8fd688e7be9cf3c456ab
김태영 <kimtaeyoung@0Taes-Mac.local>
2021-12-15 12:17:26 +0900

    footer 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,7 @@ import { NextComponentType } from 'next';
 const Footer: NextComponentType = () => {
   return (
     <footer>
-      <div
-        style={{ paddingLeft: '15px' }}
-        href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
+      <div style={{ paddingLeft: '15px' }}>
         <p style={{ fontSize: '14px' }}>
           부산광역시 금정구 서동로 91번길 7<span style={{ lineHeight: 2 }} />
         </p>
