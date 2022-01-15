dee8e5cc17e94cf39a57e4542fa423ea26e77dbb
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-01-15 12:24:59 +0900

    Seo 수정, 구글 아날리틱스 추가

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,6 +1,10 @@
 import Head from 'next/head';
 
-export default function Seo({ title }) {
+interface IProps {
+  title: string;
+}
+
+export default function Seo({ title }: IProps) {
   return (
     <Head>
       <title>{title} | 고개, 서동</title>
