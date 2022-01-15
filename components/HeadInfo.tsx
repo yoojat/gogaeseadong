f0e4b5c025bdf4f03b7c5a1de6e9d472c6b7b1ce
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-01-15 10:19:47 +0900

    타이틀 변경

--- components/HeadInfo.tsx
+++ components/HeadInfo.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 const HeadInfo = ({
-  title = '고개, 서동',
+  title = '고개 서동',
   keyword = '공유공간,  모임공간, 파티, 쿠킹클래스, 이벤트, 커플, 가족',
   contents = '공간을 업사이클링하다',
 }: Props) => {
@@ -22,7 +22,7 @@ const HeadInfo = ({
       <meta content={contents} />
       <meta name='description' content={contents} />
       <meta property='og:type' content='website' />
-      <meta property='og:title' content='고개, 서동' />
+      <meta property='og:title' content='고개 서동' />
       <meta property='og:image' content='/logo_black.png' />
       <meta
         property='og:url'
