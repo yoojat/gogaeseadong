8777d31487497f7371940857e055d81640556573
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-01-17 16:10:07 +0900

    next-seo 수정

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,16 +1,16 @@
 const config = {
-  canonical: 'https://www.gogaeseodong.ga/',
+  canonical: 'https://gogaeseodong.vercel.app/',
   openGraph: {
     type: 'website',
     locale: 'ko_KR',
-    url: 'https://www.gogaeseodong.ga/',
-    site_name: '고개, 서동',
-    title: '고개, 서동',
+    url: 'https://gogaeseodong.vercel.app/',
+    site_name: '고개 서동',
+    title: '고개 서동',
     image: {
-      url: 'https://www.gogaeseodong.ga/mainPhotos/2.JPG',
+      url: 'https://gogaeseodong.vercel.app/mainPhotos/2.JPG',
       width: 800,
       height: 600,
-      alt: '고개, 서동',
+      alt: '고개 서동',
       type: 'image/jpeg',
     },
     description: '공간을 업사이클링 하다',
