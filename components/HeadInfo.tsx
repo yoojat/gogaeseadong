706a9560eed655156c45a8d0a7fc1e965f59e5fb
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-01-28 00:10:16 +0900

    seo 수정

--- components/HeadInfo.tsx
+++ components/HeadInfo.tsx
@@ -8,13 +8,14 @@ interface Props {
 
 const HeadInfo = ({
   title = '공유주방 고개서동',
-  keyword = '공유주방, 공유공간, 모임공간, 파티, 쿠킹클래스, 이벤트, 커플, 가족',
-  contents = '공간을 업사이클링하다',
+  keyword = '부산공유주방, 금정구 공유주방, 부산모임공간, 금정구 모임공간, 부산공간대여, 금정구 공간대여, 부산베이킹공방, 금정구 베이킹공방, 부상쿠킹공방, 금정구 쿠킹공방, 부산키친공간대여, 부산키친공간, 금정구 키친공간대여, 부산파티룸, 금정구 파티룸, 부산전시공간, 금정구 전시공간',
+  contents = '부산 금정구 서동에 위치한 공유주방 겸 모임공간입니다. 사랑하는 이들과 함께 음식을 해먹으며, 소소한 추억을 나누시기 바랍니다.',
 }: Props) => {
   return (
     <Head>
       <meta charSet='UTF-8' />
       <title>{title}</title>
+      <meta name='title' content={title} />
       <meta name='keywords' content={keyword} />
       <meta name='Distribution' content='creplay' />
       <meta name='Copyright' content='creplay' />
--- next-seo.config.ts
+++ next-seo.config.ts
@@ -4,16 +4,17 @@ const config = {
     type: 'website',
     locale: 'ko_KR',
     url: 'https://gogaeseodong.vercel.app/',
-    site_name: '고개서동',
-    title: '고개서동',
+    site_name: '공유주방 고개서동',
+    title: '공유주방 고개서동',
     image: {
       url: 'https://gogaeseodong.vercel.app/mainPhotos/2.JPG',
       width: 800,
       height: 600,
-      alt: '고개서동',
+      alt: '공유주방 고개서동',
       type: 'image/jpeg',
     },
-    description: '공간을 업사이클링 하다',
+    description:
+      '부산 금정구 서동에 위치한 공유주방 겸 모임공간입니다. 사랑하는 이들과 함께 음식을 해먹으며, 소소한 추억을 나누시기 바랍니다.',
   },
 };
 
