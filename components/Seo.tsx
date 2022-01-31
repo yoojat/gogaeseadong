555d51c6cee7b16a8e330745993a79c94c4c78ad
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-02-01 00:12:39 +0900

    타이틀 추가 수정

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -7,7 +7,13 @@ interface IProps {
 export default function Seo({ title }: IProps) {
   return (
     <Head>
-      {title ? <title>{title} | 고개서동</title> : <title>고개서동</title>}
+      {title ? (
+        <title>{title} | 고개서동</title>
+      ) : (
+        <title>
+          부산 공유주방, 공간대여, 모임공간, 커뮤니티 공간 - 고개서동
+        </title>
+      )}
     </Head>
   );
 }
