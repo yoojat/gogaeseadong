c2222540ff0060ea6938e250e8d4cf1fa40268a6
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-09-29 10:26:14 +0900

    네이버 카카오톡 버튼 색상 제거

--- pages/inquery.tsx
+++ pages/inquery.tsx
@@ -9,8 +9,6 @@ const ReserveationHeaderContainer = styled.div`
 `;
 
 const KakaoButton = styled.a`
-  background-color: #f2dc02;
-  color: #371a1a;
   border: none;
   font-weight: 800;
   cursor: pointer;
@@ -21,10 +19,9 @@ const KakaoButton = styled.a`
   width: 160px;
   display: inline-block;
   padding: 10px;
+  border: 1px solid;
 `;
 const NaverButton = styled.a`
-  background-color: #02c75a;
-  color: white;
   border: none;
   font-weight: 800;
   cursor: pointer;
@@ -35,6 +32,7 @@ const NaverButton = styled.a`
   width: 160px;
   display: inline-block;
   padding: 10px;
+  border: 1px solid;
 `;
 
 const Spacer = styled.div`
